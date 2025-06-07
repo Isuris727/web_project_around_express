5436c5cc2dac4d34f992a7b3652ada839d5154c5
Isuris <ajudithsalasr@gmail.com>
2025-06-07 13:53:40 -0600

    add routes and data

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 import express from "express";
+import usersRoutes from "./routes/users.js";
+import cardsRoutes from "./routes/cards.js";
 
 const app = express();
 
 const { PORT = 3000 } = process.env;
+
+app.use("/users", usersRoutes);
+
+app.use("/cards", cardsRoutes);
+
+app.use("/", function (req, res) {
+  res.status(404).send({ message: "Recurso solicitado no encontrado" });
+});
 
 app.listen(PORT, () => {
   console.log("App ejecutandose en puerto: ", PORT);
---
+++ routes/users.js
@@ -0,0 +1,47 @@
+import express from "express";
+import fs from "fs/promises";
+import path from "path";
+const { Router } = express;
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const routes = Router();
+
+const usersPath = path.join(__dirname, "..", "data", "users.json");
+
+const fetchUsersData = async () => {
+  try {
+    const usersData = await fs.readFile(usersPath, { encoding: "utf8" });
+    return JSON.parse(usersData);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+async function getUsers(req, res) {
+  const users = await fetchUsersData();
+
+  res.json(users);
+}
+
+async function getUserById(req, res) {
+  const { id } = req.params;
+
+  const usersData = await fetchUsersData();
+
+  const foundUser = await usersData.find((user) => user._id === id);
+
+  if (foundUser !== undefined) {
+    return res.send(foundUser);
+  }
+  return res.status(404).send({ message: "ID de usuario no encontrado" });
+}
+
+routes.get("/", getUsers);
+
+routes.get("/:id", getUserById);
+
+export default routes;
